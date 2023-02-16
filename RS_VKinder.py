dfac4e26ab7b2c653f70678663367e6ba1cf79de
RushanSaldaev <saldaev.rushan@gmail.com>
2023-02-16 21:09:22 +0300

    Убрал таблицу с фотками и персданные

--- RS_VKinder.py
+++ RS_VKinder.py
@@ -6,7 +6,7 @@ from vk_api.utils import get_random_id
 from vk_api.longpoll import VkLongPoll, VkEventType 
 from operator import itemgetter
 
-from RS_dbase import create_db, clear_db, check_person, add_person, add_photo, person_list, photo_list
+from RS_dbase import create_db, clear_db, check_person, add_person
 from RS_vk_api import VK
 
 
@@ -27,6 +27,7 @@ current_search_id = 0
 
 status_dict = { 1:'не женат (не замужем)',2:'встречается',3:'помолвлен(-а)',4:'женат (замужем)',5:'всё сложно',6:'в активном поиске',7:'влюблен(-а)',8:'в гражданском браке',0:'Не указано'}
 sex_dict = {1:'Женский',2:'Мужской'}
+search_dict = {0:'Запускаем поиск... ',1:'Продолжаем поиск...'}
 
 person_count_flag = False
 hometown_flag = False
@@ -62,7 +63,16 @@ session = VK(vk_token, vk_user_id)
 
 def init_arg():
     global person_count_flag
+    global status_flag
+    global age_flag
+    global hometown_flag
+    global sex_flag
+    
     person_count_flag = False
+    status_flag = False
+    age_flag = False
+    hometown_flag = False
+    sex_flag = False
 
 def write_msg(user_id, message, attachment=None):
     vk_session.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': get_random_id(), "attachment": attachment })
@@ -80,7 +90,14 @@ def finish_work():
         clear_db(conn)
     conn.close()
     
-def start_search():
+def age_calc(bd):
+    result = 0
+    if bd != '':
+        now = datetime.datetime.now()
+        result = int(now.year) - int(str(bd.split('.')[2]))   
+    return result
+    
+def start_search(continue_flag=0):
     global server_conn
     global person_count
     global hometown
@@ -89,17 +106,52 @@ def start_search():
     global age
     global offset
     global current_search_id
+    global person_count_flag
+    global status_flag
+    global age_flag
+    global hometown_flag
+    global sex_flag
     
     current_search_id = get_random_id()
-    #print(person_count,hometown,sex,status,age,offset)
+    
+    if person_count == 0:
+        write_msg(event.user_id, "Не задано ограничение по кол-ву просматриваемых профилей!\nКакое кол-во профилей следует просмотреть за один проход?")
+        person_count_flag = True
+        return
+    
+    # если какой-то из параметров поиска не был найден в профиле, то запрашиваем его у пользователя
+    if status == 0:
+        write_msg(event.user_id, "В Вашем профиле не указано семейное положение!\nУкажите семейное положение:\n1 — не женат (не замужем),\n2 — встречается,\n3 — помолвлен(-а),\n4 — женат (замужем),\n5 — всё сложно,\n6 — в активном поиске,\n7 — влюблен(-а),\n8 — в гражданском браке")
+        status_flag = True
+        return
 
+    if age == 0:
+        write_msg(event.user_id, "В Вашем профиле не указан возраст!\nУкажите возраст для поиска:")
+        age_flag = True
+        return
+
+    if hometown == '':
+        write_msg(event.user_id, "В вашем профиле не указан родной город!\nУкажите город для поиска:")
+        hometown_flag = True
+        return
+
+    if sex not in [1,2]:
+        write_msg(event.user_id, "В вашем профиле не указан Ваш пол! Укажите пол для поиска:\n1-Женский\n2-Мужской")
+        sex_flag = True
+        return
+        
+    if not continue_flag:
+        write_msg(event.user_id, f"Параметры для поиска:\n...Город = {hometown}\n...Семейное положение = {status_dict[status]}\n...Возраст = {age}\n...Пол = {sex_dict[sex]}")
+    
+    write_msg(event.user_id, f"{search_dict[continue_flag]}")
     dataU = session.users_search(person_count, hometown, sex, status, age, offset)
     respU_dct = dataU['response']
-    #respU_cnt = respU_dct['count']
+    ixd = 0
     itemsU_list = respU_dct['items']
     if len(itemsU_list) > 0:
         with psycopg2.connect(server_conn) as conn:
             for itemU in itemsU_list:
+                ixd += 1
                 p_id = str(itemU['id'])
                 p_fname = itemU['first_name']
                 p_lname = itemU['last_name']
@@ -113,10 +165,10 @@ def start_search():
                 print(s_conn)
             
                 # проверяем пользователя по БД
-                # сохраняем в БД только в случае если пользователя нет в БД
+                # сохраняем в БД только в случае, если пользователя нет в БД
                 if not check_person(conn, p_id ):
                     # записываем в БД нового найденного пользователя
-                    add_person(conn, p_id, p_fname, p_lname, current_search_id)
+                    add_person(conn, p_id, current_search_id)
                 
                     # запрашиваем список фотографий пользователя, чтобы найти самые популярные
                     dataF = session.photos_get(p_id, 50)
@@ -125,58 +177,42 @@ def start_search():
                         print(dataF)
                         continue
                     respF_dct = dataF['response']
-                    #respF_cnt = respF_dct['count']
                     itemsF_list = respF_dct['items']
-                    
-                    write_msg(event.user_id, "Что-то нашли...")
-                    
+
                     lst_tmp = []
                     if len(itemsF_list) > 0:
+                        # выводим результат поиска только если есть хотя бы одна фотка
+                        write_msg(event.user_id, f"==========================\n{ixd}: {p_fname} {p_lname}, профиль: https://vk.com/id{p_id} ") 
+                        
                         for itemF in itemsF_list:
                             d_tmp = {}
                             d_tmp['ph_id'] = itemF['id']
                             d_tmp['ph_owner_id'] = itemF['owner_id']
                             d_tmp['ph_likes'] = itemF['likes']['count']
                             d_tmp['ph_comments'] = itemF['comments']['count']
-                            p_sizes_list = itemF['sizes']
-                            if len(p_sizes_list) > 0:
-                                # p_sizes = p_sizes_list[0]
-                                d_tmp['ph_height'] = p_sizes_list[0]['height']
-                                d_tmp['ph_width'] = p_sizes_list[0]['width']
-                                d_tmp['ph_type'] = p_sizes_list[0]['type']
-                                d_tmp['ph_url'] = p_sizes_list[0]['url']
                             # добавляем словарь с список    
                             lst_tmp.append(d_tmp)
                         # сортируем список по лайкам
                         lst_srt = sorted(lst_tmp, key=itemgetter('ph_likes'), reverse=True)
                         
+                        ixp = 0
                         for el in lst_srt[:3]:
-                            # записываем в БД три фото найденного пользователя
-                            add_photo(conn, p_id, el['ph_owner_id'], el['ph_id'], el['ph_height'], el['ph_width'], el['ph_type'], el['ph_url'], el['ph_likes'], el['ph_comments'])                        
-    conn.close()    
+                            # не записывая в БД сразу показываем до трех фото найденного пользователя, 
+                            # с максимальным количеством лайков
+                   
+                            ixp += 1
+                            attachments = []
+                            attachments.append('photo{}_{}'.format(el['ph_owner_id'], el['ph_id']))
+                            attachment=','.join(attachments)
+                            write_msg(event.user_id, f"фото {ixp}: кол-во лайков = {el['ph_likes']}, кол-во комментов = {el['ph_comments']}",attachment)
+                            
+    conn.close() 
+    write_msg(event.user_id, "Поиск завершён...")
 
-def result_search(search_id):
-    global server_conn
-    #print(server_conn)
-    prs_list = []
-    with psycopg2.connect(server_conn) as conn:
-        prs_list = person_list(conn, search_id)
-    conn.close()      
-    return prs_list
-
-def photos_get(p_id):
-    global server_conn
-    photos_list = []
-    with psycopg2.connect(server_conn) as conn:
-        photos_list = photo_list(conn, p_id)
-    conn.close()      
-    return photos_list
-    
+# основной цикл проверки сообщений    
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
                
-        #print(person_count,hometown,sex,status,age,offset)
-
         if event.to_me:
             msg = event.text.lower()
             
@@ -185,21 +221,42 @@ for event in longpoll.listen():
                 if person_count < 0 or person_count > 100:
                     person_count = 100
                     write_msg(event.user_id, "Ограничиваем кол-во просматриваемых профилей = 100 профилей!")
-                
-                dataP = session.getProfileInfo()
-                respP_dct = dataP['response']
-                hometown = respP_dct['home_town'] if 'home_town' in respP_dct else ''
-                status = int(respP_dct['relation']) if 'relation' in respP_dct else 1
-                sex = int(respP_dct['sex']) if 'sex' in respP_dct else 1
-                bdate = respP_dct['bdate'] if 'bdate' in respP_dct else ''
-                if bdate != '':
-                    # текущая дата
-                    now = datetime.datetime.now()
-                    # возраст
-                    age = int(now.year) - int(str(bdate.split('.')[2]))
-
-                write_msg(event.user_id, f"Параметры для поиска:\n...Город = {hometown}\n...Семейное положение = {status_dict[status]}\n...Возраст = {age}\n...Пол = {sex_dict[sex]}\n\nСтарт - для запуска поиска...")
                 person_count_flag = False
+                start_search(0)
+                continue
+
+            if status_flag:
+                status = int(msg)
+                if (status < 1 or status > 8):
+                    status = 1
+                    write_msg(event.user_id, "Ограничиваем поиcк семейным положением не женат (не замужем)!")
+                status_flag = False
+                start_search(0)
+                continue    
+
+            if age_flag:
+                age = int(msg)
+                if age < 0 or age > 100:
+                    age = 25
+                    write_msg(event.user_id, "Ограничиваем поиcк возрастом в 25 лет!")
+                age_flag = False
+                continue
+
+
+            if hometown_flag:
+                hometown = msg
+                if hometown == '':
+                    hometown = 'Москва'
+                    write_msg(event.user_id, "Ограничиваем поиск по городу Москва!")
+                hometown_flag = False
+                continue
+
+            if sex_flag:
+                sex = int(msg)
+                if sex not in [1,2]:
+                    sex = 1
+                    write_msg(event.user_id, "Ограничиваем поиск женским полом!")
+                sex_flag = False
                 continue
 
 
@@ -211,29 +268,24 @@ for event in longpoll.listen():
             elif msg == "начать":
                 start_work()
                 write_msg(event.user_id, "Создание БД перед началом запросов...")
-                write_msg(event.user_id, "Какое кол-во профилей следует просмотреть?")
-                person_count_flag = True
-            elif msg in ["старт","еще","продолжить" ]:
-                if msg in ["еще","продолжить" ]: 
-                    offset = offset + person_count
-                    write_msg(event.user_id, "Старт поиска...")
-                start_search()
-                write_msg(event.user_id, "Поиск завершён...")
-            elif msg == "результат":
-                rest_list = result_search(current_search_id)
-                #print(rest_list)
-                ixd = 0
-                for item in rest_list:
-                    ixd += 1
-                    write_msg(event.user_id, f"==========================\n{ixd}: {item['fname']} {item['lname']}, профиль: https://vk.com/id{item['vkid']} ")                                                
-                    ph_list = photos_get(item['vkid'])
-                    #print('...',ph_list)
-                    ixp = 0
-                    for ph in ph_list:
-                        ixp += 1
-                        attachments = []
-                        attachments.append('photo{}_{}'.format(ph['ph_owner_id'], ph['ph_id']))
-                        attachment=','.join(attachments)
-                        write_msg(event.user_id, f"фото {ixp}: кол-во лайков = {ph['ph_likes']}, кол-во комментов = {ph['ph_comments']}",attachment)
+                
+                # получаем всю необходимую информацию из профиля пользователя
+                dataP = session.getProfileInfo()
+                respP_dct = dataP['response']
+                hometown = respP_dct['home_town'] if 'home_town' in respP_dct else ''
+                status = int(respP_dct['relation']) if 'relation' in respP_dct else 1
+                sex = int(respP_dct['sex']) if 'sex' in respP_dct else 0
+                # меняем пол на противоположный (для более адекватного поиска)
+                sex = 1 if sex == 2 else 1 
+                bdate = respP_dct['bdate'] if 'bdate' in respP_dct else ''
+                age = age_calc(bdate)
+                
+                write_msg(event.user_id, "'старт' - для запуска функции поиска. ")
+                write_msg(event.user_id, "'еще' или 'продолжить' - для продолжения поиска по текущим параметрам. ")
+            elif msg == "старт":
+                start_search(0)
+            elif msg in ["еще","продолжить" ]:
+                offset = offset + person_count
+                start_search(1)
             else:
                 write_msg(event.user_id, "Не понял вашего вопроса...")
--- RS_dbase.py
+++ RS_dbase.py
@@ -6,22 +6,7 @@ def create_db(conn):
             CREATE TABLE IF NOT EXISTS vk_person (
                 id SERIAL PRIMARY KEY,
                 vk_person_id INTEGER NOT NULL, 
-                last_name VARCHAR(100) NOT NULL,
-                first_name VARCHAR(50) NOT null,
                 search_id INTEGER DEFAULT 0
-            );
-
-            CREATE TABLE IF NOT EXISTS vk_photos (
-                id SERIAL PRIMARY KEY,
-                vk_person_id INTEGER NOT NULL, 
-                ph_owner_id INTEGER DEFAULT 0, 
-                ph_id INTEGER DEFAULT 0,
-                ph_height INTEGER DEFAULT 0,
-                ph_width INTEGER DEFAULT 0,
-                ph_type VARCHAR(1) DEFAULT '',
-                ph_url VARCHAR(500) DEFAULT '',
-                ph_likes INTEGER DEFAULT 0,
-                ph_comments INTEGER DEFAULT 0
             );
 
         """)
@@ -33,7 +18,6 @@ def create_db(conn):
 def drop_db(conn):
     with conn.cursor() as cur:
         cur.execute("""
-            DROP TABLE vk_photos;
             DROP TABLE vk_person;
         """)
         conn.commit()
@@ -42,7 +26,6 @@ def drop_db(conn):
 def clear_db(conn):
     with conn.cursor() as cur:
         cur.execute("""
-            DELETE FROM vk_photos;
             DELETE FROM vk_person;
         """)
         conn.commit()
@@ -52,7 +35,7 @@ def check_person(conn, p_id):
     result = False
     with conn.cursor() as cur:
         sql_query = f"""
-        SELECT id, vk_person_id, first_name, last_name
+        SELECT id, vk_person_id
         FROM vk_person
         WHERE vk_person_id={p_id};
         """
@@ -63,86 +46,17 @@ def check_person(conn, p_id):
 
     return result
 
-
-def add_person(conn, p_id, first_name, last_name, search_id):
+def add_person(conn, p_id, search_id):
     with conn.cursor() as cur:
         sql_query = f"""
-        INSERT INTO vk_person(vk_person_id, first_name, last_name, search_id) 
-        VALUES( {p_id}, '{first_name}', '{last_name}', {search_id} );
+        INSERT INTO vk_person(vk_person_id, search_id) 
+        VALUES( {p_id}, {search_id} );
         """        
         cur.execute(sql_query)
         conn.commit()    
 
-def add_photo(conn, p_id, ph_owner_id, ph_id, ph_height, ph_width, ph_type, ph_url, ph_likes, ph_comments):
-    with conn.cursor() as cur:
-        sql_query = f"""
-        INSERT INTO vk_photos(vk_person_id, ph_owner_id, ph_id, ph_height, ph_width, ph_type, ph_url, ph_likes, ph_comments) 
-        VALUES( {p_id}, {ph_owner_id}, {ph_id}, '{ph_height}', '{ph_width}', '{ph_type}', '{ph_url}',  '{ph_likes}', '{ph_comments}' );
-        """        
-        cur.execute(sql_query)
-        conn.commit()    
-
-
-def person_list(conn, search_id):
-    lst_tmp = []
-    with conn.cursor() as cur:
-        cur.execute(f"""
-        with 
-        rest1 as ( select * from vk_person where search_id = {search_id}),
-        rest2 as ( 
-            select 
-            vk_person_id, 
-            count(*) as ph_cnt, 
-            sum(ph_likes) as ph_sum_likes
-            from vk_photos
-            group by vk_person_id 
-        )
-        select rest1.id, rest1.vk_person_id, rest1.last_name, rest1.first_name, rest2.ph_cnt, rest2.ph_sum_likes
-        from rest1
-        inner join rest2
-            on rest1.vk_person_id = rest2.vk_person_id
-        ;
-        """)
-        for person in cur.fetchall():
-            sid, vkid, lname, fname, vkcnt, vksumlikes = person
-            d_tmp = {}
-            d_tmp['sid']=sid
-            d_tmp['vkid']=vkid
-            d_tmp['lname']=lname
-            d_tmp['fname']=fname
-            d_tmp['vkcnt']=vkcnt
-            d_tmp['vksumlikes']=vksumlikes
-            lst_tmp.append(d_tmp)
-    return lst_tmp
             
-
-def photo_list(conn, p_id):
-    lst_tmp = []
-    with conn.cursor() as cur:
-        cur.execute(f"""
-        select 
-          vp.vk_person_id, 
-          vp.ph_owner_id,
-          vp.ph_id, 
-          vp.ph_url, 
-          vp.ph_type,
-          vp.ph_likes,
-          vp.ph_comments
-        from vk_photos vp 
-        where vk_person_id = {p_id};
-        """)
-        for photo in cur.fetchall():
-            ph_vkid, ph_ownid, ph_id, ph_url, ph_type, ph_likes, ph_comments = photo
-            d_tmp = {}
-            d_tmp['ph_vkid']=ph_vkid
-            d_tmp['ph_owner_id']=ph_ownid
-            d_tmp['ph_id']=ph_id
-            d_tmp['ph_url']=ph_url
-            d_tmp['ph_type']=ph_type
-            d_tmp['ph_likes']=ph_likes
-            d_tmp['ph_comments']=ph_comments
-            lst_tmp.append(d_tmp)
-    return lst_tmp            
+      
 
             
       
